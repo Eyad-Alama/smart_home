ad89a519a16130157fa7ab67804086cb8d75a42a
Eyad Alama <eyad@Eyads-MacBook-Pro.local>
2017-12-18 18:16:59 +0800

    added yeelight library

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from rotator import getRotation
 from active_buzzer import buzzer
 from laser import laser_freq, laser_on, laser_off, laser_switch
+from yeelight import set_bright_all
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -16,14 +17,16 @@ btn_status = False
 
 
 while True:
-	status = button_read(37)
-	if status >0:
-		if btn_status == False:	
-			btn_status = True
-		else:
-			btn_status = False
-		btn_switch(btn_status)
+
+	# status = button_read(37)
+	# if status >0:
+	# 	if btn_status == False:	
+	# 		btn_status = True
+	# 	else:
+	# 		btn_status = False
+	# 	btn_switch(btn_status)
 	#laser_freq(32,0.3)
-#	level = getRotation(38,36)
+	level = getRotation(38,36)
+	set_bright_all(level)
 #	print level
 	#buzzer(29,3)
